f39446036d64c09c61e234ba7d361c8bc08ed550
RafaelAlcantara1 <151959388+RafaelAlcantara1@users.noreply.github.com>
2025-03-07 15:36:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const input = document.getElementById("nomeInput");
-    const button = document.getElementById("avaliarButton");
-    const resultado = document.getElementById("resultado");
-    const progressBar = document.getElementById("progressBar");
-    const imagemFalsiane = document.getElementById("imagemFalsiane");
-  
-    button.addEventListener("click", function () {
-      let nome = input.value.trim().toLowerCase();
-      let nivelFalsidade;
-  
-      if (nome === "mariani" || "isabela") {
-        nivelFalsidade = 100;
-      } else if (nome === "rafael" || "rafael henrique rodrigues de alcantara") {
-        nivelFalsidade = 0;
-      } else {
-        nivelFalsidade = Math.floor(Math.random() * 101);
-      }
-  
-      resultado.innerText = `${input.value} tem ${nivelFalsidade}% de falsidade!`;
-      progressBar.value = nivelFalsidade;
-  
-      if (nivelFalsidade > 75) {
-        imagemFalsiane.style.display = "block";
-      } else {
-        imagemFalsiane.style.display = "none";
-      }
-    });
+  const input = document.getElementById("nomeInput");
+  const button = document.getElementById("avaliarButton");
+  const resultado = document.getElementById("resultado");
+  const progressBar = document.getElementById("progressBar");
+  const imagemFalsiane = document.getElementById("imagemFalsiane");
+
+  button.addEventListener("click", function () {
+    let nome = input.value.trim().toLowerCase();
+    let nivelFalsidade;
+
+    if (nome === "mariani" || "isabela") {
+      nivelFalsidade = 100;
+    } else if (nome === "rafael") {
+      nivelFalsidade = 0;
+    } else {
+      nivelFalsidade = Math.floor(Math.random() * 101);
+    }
+
+    resultado.innerText = `${input.value} tem ${nivelFalsidade}% de falsidade!`;
+    progressBar.value = nivelFalsidade;
+
+    if (nivelFalsidade > 75) {
+      imagemFalsiane.style.display = "block";
+    } else {
+      imagemFalsiane.style.display = "none";
+    }
   });
+});
